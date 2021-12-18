5103b9d7efa63be928bc89a819a5ab45857ca964
ethan <ethanhochy@gmail.com>
2021-12-18 00:13:11 +0000

    initial basic Sudoku solver complete

--- Basic_Sudoku.py
+++ Basic_Sudoku.py
@@ -1,15 +1,16 @@
 # initial basic Sudoku solver
 # use back tracking algorithm
+
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
 
@@ -25,42 +26,42 @@ board = [
 #     [7,9,1,0,5,0,6,0,8]
 # ]
 
-def print_board(board):
+def print_board(boa):
     # i is row
-    for i in range(len(board)):
+    for i in range(len(boa)):
         # j is column
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
-        for j in range(len(board[0])):
+        for j in range(len(boa[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end='')
-            if j == len(board[0]) - 1:
-                print(board[i][j])
+            if j == len(boa[0]) - 1:
+                print(boa[i][j])
             else:
-                print(str(board[i][j]) + ' ', end='')
+                print(str(boa[i][j]) + ' ', end='')
 
 
 # find the next empty position in the board
 # go from left to right, top to bottom
-def find_next(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)
+def find_next(boa):
+    for i in range(len(boa)):
+        for j in range(len(boa[0])):
+            if boa[i][j] == 0:
+                return i, j
     return None
+
 
 # determine if input is valid
 # position inputted is (row, column)
-def valid(board, position, number):
-
+def valid(boa, position, number):
     # check row so go through columns of the row
-    for j in range(len(board[0])):
-        if board[position[0]][j] == number and j != position[1]:
+    for j in range(len(boa[0])):
+        if boa[position[0]][j] == number and j != position[1]:
             return False
 
     # check column so go through rows of the column
-    for i in range(len(board)):
-        if board[i][position[1]] == number and i != position[0]:
+    for i in range(len(boa)):
+        if boa[i][position[1]] == number and i != position[0]:
             return False
 
     # check 3x3 box
@@ -69,33 +70,33 @@ def valid(board, position, number):
     y = position[1] // 3
     for i in range(3):
         for j in range(3):
-            if board[3 * x + i][3 * y + j] == number and (i, j) != position:
+            if boa[3 * x + i][3 * y + j] == number and (i, j) != position:
                 return False
 
     return True
 
+
 # solve the Sudoku
 # use backtracking algorithm
-def solve(board):
+def solve(boa):
 
-    find = find_next(board)
+    find = find_next(boa)
     # base case where board is full
     if not find:
         return True
     else:
         x, y = find
         # try out each possibility
-        for i in range(1, len(board) + 1):
-            if valid(board, (x, y), i):
-                board[x][y] = i
-                if solve(board):
+        for i in range(1, len(boa) + 1):
+            if valid(boa, (x, y), i):
+                boa[x][y] = i
+                # print_board(boa)
+                if solve(boa):
                     return True
-                # backtrack if cannot solve - recursion
-                board[x][y] = 0
+                # if it cannot solve backtrack - recursion
+                boa[x][y] = 0
+                # print_board(boa)
         return False
-
-
-
 
 
 print_board(board)
@@ -105,4 +106,3 @@ else:
     print("-----------------------------------")
     print_board(board)
     # print(valid(board, find_next(board), 3))
-
